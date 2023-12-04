c1dea082cf9bf40a0cc4a17db47e65b008bb2cde
MSDuran <sametduran95@gmail.com>
2023-12-04 23:50:36 +0300

    code fix

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import multiprocessing
 from utils import Utils
 from sklearn.model_selection import train_test_split
@@ -19,6 +20,7 @@ user_item_matrix = ratings.pivot(
     columns='user_id',
     values='rating'
 )
+
 # Median Imputation (MD) method
 user_item_matrix = user_item_matrix.apply(lambda x: x.fillna(x.median()), axis=0)
 
@@ -27,6 +29,13 @@ sparse_matrix = csr_matrix(user_item_matrix.values)
 dbscan = DBSCAN(eps=0.5, min_samples=5, metric='euclidean', n_jobs=-1)
 user_item_matrix_np = sparse_matrix.toarray()
 clusters = dbscan.fit_predict(user_item_matrix_np.T)
+
+user_cluster_ids = pd.Series(clusters, index=user_item_matrix.columns).reset_index()
+user_cluster_ids.columns = ['user_id', 'cluster_id']
+
+users = users.merge(user_cluster_ids, on='user_id')
+
+ratings = ratings.merge(users[['user_id', 'cluster_id']], on='user_id')
 
 train_data, test_data = train_test_split(ratings, test_size=0.2, random_state=7)
 
@@ -44,12 +53,12 @@ model_catboost = CatBoostRegressor(
 )
 
 model_catboost.fit(
-    train_data[['user_id', 'movie_id']],
+    train_data[['user_id', 'movie_id', 'cluster_id']],
     train_data['rating'],
     verbose=False
 )
 
-predictions = model_catboost.predict(test_data[['user_id', 'movie_id']])
+predictions = model_catboost.predict(test_data[['user_id', 'movie_id', 'cluster_id']])
 rmse = mean_squared_error(test_data['rating'], predictions, squared=False)
 mae = mean_absolute_error(test_data['rating'], predictions)
 
